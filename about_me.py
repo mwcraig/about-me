9dd3da15fc7c28033931ce0cc065670d7651c392
Matthew Craig <mattwcraig@gmail.com>
2016-10-30 15:03:36 -0500

    Add initial version of code

---
+++ about_me.py
@@ -0,0 +1,13 @@
+from __future__ import print_function
+
+print("My name is Andy and I    ")
+
+print("My name is Jane and I    ")
+
+print("My name is Laura and I   ")
+
+print("My name is Erin and I    ")
+
+print("My name is Elias and I   ")
+
+print("My name is Stefan and I don't matter")
