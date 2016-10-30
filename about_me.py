c4ddb9a8bc9804f5ed8821b78f136a46fb8a9947
Matthew Craig <mattwcraig@gmail.com>
2016-10-30 15:26:08 -0500

    Add myself to the about_me file

--- about_me.py
+++ about_me.py
@@ -11,3 +11,5 @@ print("My name is Erin and I    ")
 print("My name is Elias and I   ")
 
 print("My name is Stefan and I don't matter")
+
+print("Me name is Matt and I wasn't even listed initially :(")
